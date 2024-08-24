967c10befa90ec470f7e7902ff7fb9feeb1f8169
Shuken <shuken@gmail.com>
2024-08-24 21:13:18 +0200

    Update script3.js

--- script3.js
+++ script3.js
@@ -3,24 +3,24 @@ let tokenExpiry = 0;
 
 const getToken = async () => {
   const response = await fetch("https://oauth2.cex.es.webuy.io/token", {
-    headers: {
-      "accept": "application/json, text/plain, */*",
-      "accept-language": "es-ES,es;q=0.9,ca;q=0.8,en;q=0.7",
-      "content-type": "application/x-www-form-urlencoded",
-      "priority": "u=1, i",
-      "sec-ch-ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Google Chrome\";v=\"126\"",
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": "\"Windows\"",
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "cross-site"
-    },
-    referrer: "https://es.webuy.com/user/account?tab=sales&page=1&slotName=activo",
-    referrerPolicy: "no-referrer-when-downgrade",
-    body: "client_id=cexweb&client_secret=18733fb8b6aa2bcbf17a2d0eba7483e6&grant_type=refresh_token&refresh_token=01ddd7205123dae5cf2e31a85bf7c40bdf445d65",
-    method: "POST",
-    mode: "cors",
-    credentials: "omit"
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "es-ES,es;q=0.9,ca;q=0.8,en;q=0.7",
+    "content-type": "application/x-www-form-urlencoded",
+    "priority": "u=1, i",
+    "sec-ch-ua": "\"Chromium\";v=\"128\", \"Not;A=Brand\";v=\"24\", \"Google Chrome\";v=\"128\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "cross-site"
+  },
+  "referrer": "https://es.webuy.com/user/account?tab=sales&page=1&slotName=activo",
+  "referrerPolicy": "no-referrer-when-downgrade",
+  "body": "client_id=cexweb&client_secret=18733fb8b6aa2bcbf17a2d0eba7483e6&grant_type=refresh_token&refresh_token=6fae10047693fac64962cb0f2f1a0246d4286f89",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "omit"
   });
   console.log(response);
   if (!response.ok) {
