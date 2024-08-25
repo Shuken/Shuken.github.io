4830051942214c64ad1bd846752b6f5ecc8cc133
Shuken <shuken@gmail.com>
2024-08-25 19:59:54 +0200

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,6 +1,6 @@
 let token = null;
 let tokenExpiry = 0;
-let refreshToken = "5694bffc12418dfb15da6540937745f0e2d6aba6";
+let refreshToken = "2c7ab880b5e74a7041fffb23eb07a15e7df79202";
 
 const getToken = async () => {
   const response = await fetch("https://oauth2.cex.es.webuy.io/token", {
@@ -40,25 +40,24 @@ const getElements = async () => {
     await getToken();
   }
 
-  const response = await fetch("https://wss2.cex.es.webuy.io/v3/members/408026/favouriteboxes?accessToken=" + token.toString() + "&firstRecord=1&count=6&sortBy=favouriteBoxAddedTime&sortOrder=desc", {
-    headers: {
-      "accept": "application/json, text/plain, */*",
-      "accept-language": "es-ES,es;q=0.9,ca;q=0.8,en;q=0.7",
-      "priority": "u=1, i",
-      "sec-ch-ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Google Chrome\";v=\"126\"",
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": "\"Windows\"",
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "cross-site"
-    },
-    referrer: "https://es.webuy.com/user/account?tab=favourites&page=1&sortBy=most-recent",
-    referrerPolicy: "no-referrer-when-downgrade",
-    body: null,
-    method: "GET",
-    mode: "cors",
-    credentials: "omit"
-  });
+  const response = await fetch("https://wss2.cex.es.webuy.io/v3/members/95982/favouriteboxes?accessToken=" + token.toString() + "&firstRecord=1&count=6&sortBy=favouriteBoxAddedTime&sortOrder=desc", {
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+    "sec-ch-ua": "\"Chromium\";v=\"128\", \"Not;A=Brand\";v=\"24\", \"Google Chrome\";v=\"128\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\""
+  },
+  "referrer": "https://es.webuy.com/user/account?tab=favourites&page=1&sortBy=most-recent",
+  "referrerPolicy": "no-referrer-when-downgrade",
+  "body": null,
+  "method": "GET",
+  "mode": "cors",
+  "credentials": "omit"
+});
+  
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
 
   if (!response.ok) {
     throw new Error("Failed to fetch elements");
